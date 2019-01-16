7d8ea8f96bfc133fbe5208e24a0d1dfc1191a964
Robert Zhang <louirobert@gmail.com>
2019-01-16 11:44:02 +0800

    Make a base class for all Acm commands.

---
+++ src/AcmCommand.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using HPC.ACM.API;
+
+namespace HPC.ACM.API.PS
+{
+    public class AcmCommand : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true
+        )]
+        public Connection Connection { get; set; }
+
+        protected HPCWebAPI ApiClient { get; set; }
+
+        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+        protected override void BeginProcessing()
+        {
+            ApiClient = ApiClientFactory.Create(Connection);
+        }
+    }
+}
--- src/GetAcmNodeCommand.cs
+++ src/GetAcmNodeCommand.cs
@@ -7,22 +7,8 @@ namespace HPC.ACM.API.PS
 {
     [Cmdlet("Get", "AcmNode")]
     [OutputType(typeof(API.Models.Node))]
-    public class GetAcmNodeCommand : PSCmdlet
+    public class GetAcmNodeCommand : AcmCommand
     {
-        [Parameter(
-            Mandatory = true,
-            ValueFromPipelineByPropertyName = true
-        )]
-        public Connection Connection { get; set; }
-
-        private HPCWebAPI ApiClient { get; set; }
-
-        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
-        protected override void BeginProcessing()
-        {
-            ApiClient = ApiClientFactory.Create(Connection);
-        }
-
         // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
         protected override void ProcessRecord()
         {
